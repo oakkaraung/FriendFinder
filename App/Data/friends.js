560bca2b2cddb60201a05a57516bf5018a10d329
Oakkar <oakkaraung1659@gmail.com>
2019-06-30 14:37:08 -0700

    adjusted some values

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"James",
+        photo:"https://image.shutterstock.com/mosaic_250/580987/439360372/stock-photo-happy-young-businessman-with-tablet-winking-and-showing-ok-sign-over-white-background-439360372.jpg",
         scores:[
             5,
             1,
@@ -19,7 +19,7 @@ var friends = [
           name: "Oakkar",
           photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/60109049_2320798984646659_3389891865665863680_n.jpg?_nc_cat=105&_nc_oc=AQnt5vP9x9dpleoru88HDm9KckSPcOL4F_0AoEAr6YNNLipvfo4nEQjbWCOWUP-_BeE&_nc_ht=scontent-lax3-1.xx&oh=c578b4c721b8041da0cb2d350428bcd9&oe=5DBE2C05",
           scores: [
-              1,2,3,4,5,5,4,3,2,1
+              2,5,5,4,4,1,2,2,3,1
           ]
       }
 ]
